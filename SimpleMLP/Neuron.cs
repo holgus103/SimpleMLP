e5f9d07272ed6d7fe728ec9d544365da7fe0a1cf
holgus103 <kubasuchan@hotmail.com>
2017-10-11 01:06:30 +0200

    Initial commit

---
+++ SimpleMLP/Neuron.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMLP
+{
+    public partial class Network
+    {
+        private class Neuron
+        {
+            public double Output => this.activate(this.calculateNetInput());
+            private List<Tuple<Neuron, double>> predecessors = new List<Tuple<Neuron, double>>();
+            private double calculateNetInput() => this.predecessors.Aggregate(0.0, (s, t) => s + t.Item1.Output * t.Item2);
+            // activationFunction
+            private double activate(double val) => 1 / (1 + Math.Exp(-val));
+        }
+    }
+
+}

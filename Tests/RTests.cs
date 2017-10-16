46ba6261f9e2071d0965a79a0d34ee317a71e5e8
holgus103 <kubasuchan@hotmail.com>
2017-10-16 17:05:29 +0200

    R Chart Test

---
+++ Tests/RTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class RTests
+    {
+        private static REngine engine;
+
+        [ClassInitialize]
+        public static void Init(TestContext ctx)
+        {
+            REngine.SetEnvironmentVariables(); // <-- May be omitted; the next line would call it.
+            engine = REngine.GetInstance();
+        }
+
+        [TestMethod]
+        public void RTest()
+        {
+
+            // A somewhat contrived but customary Hello World:
+            CharacterVector charVec = engine.CreateCharacterVector(new[] { "Hello, R world!, .NET speaking" });
+            engine.SetSymbol("greetings", charVec);
+            engine.Evaluate("str(greetings)"); // print out in the console
+            string[] a = engine.Evaluate("'Hi there .NET, from the R engine'").AsCharacter().ToArray();
+            Console.WriteLine("R answered: '{0}'", a[0]);
+            Console.WriteLine("Press any key to exit the program");
+        }
+
+        [TestMethod]
+        public void DrawCharRTest()
+        {
+            var x = engine.CreateIntegerVector(new[] { 1, 2, 3 });
+            var y = engine.CreateIntegerVector(new[] { 1, 2, 3 });
+            engine.SetSymbol("x", x);
+            engine.SetSymbol("y", y);
+            engine.Evaluate("plot(x,y)");
+        }
+
+        [ClassCleanup]
+        public static void Teardown()
+        {
+            engine.Dispose();
+        }
+    }
+}

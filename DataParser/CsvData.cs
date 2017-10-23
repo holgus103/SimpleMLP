dd79839fc63c857c602a98c7fc3afa64459eb2ec
holgus103 <kubasuchan@hotmail.com>
2017-10-24 00:15:10 +0200

    Appending labels to data - preserves original values before normalization

--- DataParser/CsvData.cs
+++ DataParser/CsvData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ namespace DataParser
         List<Tuple<double, double>> attributeSpansAndShifts;
 
         public List<Tuple<List<double>, List<double>>> NormalizedData => this.normalizedData;
+        public List<Tuple<List<double>, List<double>>> RawData => this.data;
 
 
         public CsvData() { }
